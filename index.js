dbea27d3ede645f59cf0668c64d7a6b7eaf835b0
maclaine <maclainekuria@gmail.com>
2022-06-05 18:41:21 +0300

    commit tree 58c3d56bb00af577719d7401fe761d6d1049b698

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  "Hello!".toUpperCase(); // 'HELLO!'
+
+  function logShout(string) {
+    return string.toLowerCase();
+  }
+  
+  "sayHiToHeadphonedRoommate".toLowerCase();
